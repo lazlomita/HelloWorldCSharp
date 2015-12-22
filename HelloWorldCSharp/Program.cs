0dda4b785d7bf221c857f96d7a6692b8946ada16
Lazlo Mita <lazlo.mita@gmail.com>
2015-12-22 14:16:31 -0400

    Add the Hello World code

--- HelloWorldCSharp/Program.cs
+++ HelloWorldCSharp/Program.cs
@@ -10,6 +10,8 @@ namespace HelloWorldCSharp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World C#");
+            Console.ReadLine();
         }
     }
 }
